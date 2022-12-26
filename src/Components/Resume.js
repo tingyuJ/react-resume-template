43d4ca4b3d94f1c493241a816964296ea63fcc78
Tingyu <jiang.tingyu19@gmail.com>
2022-12-26 19:03:45 +0800

    Update

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -3,77 +3,88 @@ import React, { Component } from 'react';
 class Resume extends Component {
   render() {
 
-    if(this.props.data){
+    if (this.props.data) {
       var skillmessage = this.props.data.skillmessage;
-      var education = this.props.data.education.map(function(education){
+      var education = this.props.data.education.map(function (education) {
         return <div key={education.school}><h3>{education.school}</h3>
-        <p className="info">{education.degree} <span>&bull;</span><em className="date">{education.graduated}</em></p>
-        <p>{education.description}</p></div>
+          <p className="info">{education.degree} <span>&bull;</span><em className="date">{education.graduated}</em></p>
+          <p>{education.description}</p></div>
       })
-      var work = this.props.data.work.map(function(work){
+      var work = this.props.data.work.map(function (work) {
         return <div key={work.company}><h3>{work.company}</h3>
-            <p className="info">{work.title}<span>&bull;</span> <em className="date">{work.years}</em></p>
-            <p>{work.description1}
-            <br/>{work.description2}
-            <br/>{work.description3}</p>
+          <p className="info">{work.title}<span>&bull;</span> <em className="date">{work.years}</em></p>
+          <p>{work.description1}
+            <br />{work.description2}
+            <br />{work.description3}</p>
         </div>
       })
-      var skills = this.props.data.skills.map(function(skills){
-        var className = 'bar-expand '+skills.name.toLowerCase();
-        return <li key={skills.name}><span style={{width:skills.level}}className={className}></span><em>{skills.name}</em></li>
+      var skills = this.props.data.skills.map(function (skills) {
+        var className = 'bar-expand ' + skills.name.toLowerCase();
+        return <li key={skills.name}><span style={{ width: skills.level }} className={className}></span><em>{skills.name}</em></li>
       })
     }
 
     return (
       <section id="resume">
 
-      <div className="row education">
-         <div className="three columns header-col">
+        <div className="row education">
+          <div className="three columns header-col">
             <h1><span>Education</span></h1>
-         </div>
+          </div>
 
-         <div className="nine columns main-col">
+          <div className="nine columns main-col">
             <div className="row item">
-               <div className="twelve columns">
-                 {education}
-               </div>
+              <div className="twelve columns">
+                {education}
+              </div>
             </div>
-         </div>
-      </div>
-
-
-      <div className="row work">
-
-         <div className="three columns header-col">
-            <h1><span>Work</span></h1>
-         </div>
-
-         <div className="nine columns main-col">
-          {work}
+          </div>
         </div>
-    </div>
+
+
+        <div className="row work">
+
+          <div className="three columns header-col">
+            <h1><span>Work</span></h1>
+          </div>
+
+          <div className="nine columns main-col">
+            <div><h3>Systex</h3>
+              <p className="info">Full Stack Developer<span>&bull;</span> <em className="date">2022/02 - present</em></p>
+              <p>- Build maintainable/scalable web applications/libraries with third-party software integration.
+                <br />- Worked on 5+ corporate-scaled MVC web apps with layered architecture.
+                <br />- Front-end development using Angular (rxjs, boostrap)/Razor Page (jQuery, AJAX).
+                <br />- Develope back-end solutions with ASP.NET MVC/Web API (.NET Core, .NET 6, Entity Framwork, LINQ, RESTful API).
+                <br />- Write stored procedures. (MSSQL)
+                <br />- Initiated and organized a study group.
+                <br />- Successfully lead the integration and implementation of git branches in a team of 7 developers.
+              </p>
+            </div>
+            {work}
+          </div>
+        </div>
 
 
 
-      <div className="row skill">
+        <div className="row skill">
 
-         <div className="three columns header-col">
+          <div className="three columns header-col">
             <h1><span>Skills</span></h1>
-         </div>
+          </div>
 
-         <div className="nine columns main-col">
+          <div className="nine columns main-col">
 
             <p>{skillmessage}
             </p>
 
-				<div className="bars">
-				   <ul className="skills">
-					  {skills}
-					</ul>
-				</div>
-			</div>
-      </div>
-   </section>
+            <div className="bars">
+              <ul className="skills">
+                {skills}
+              </ul>
+            </div>
+          </div>
+        </div>
+      </section>
     );
   }
 }
